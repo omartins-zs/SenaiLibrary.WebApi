c14f650807ac71524a3f0ed6cb2ede4f5b635e88
omartins-zs <gabrielmatheus209@gmail.com>
2022-02-05 18:03:32 -0300

    Criando Repositorio Jogo e Metodo Get

---
+++ SenaiLibrary.WebApi/Repositories/JogoRepository.cs
@@ -0,0 +1,26 @@
+﻿using SenaiLibrary.WebApi.Contexts;
+using SenaiLibrary.WebApi.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SenaiLibrary.WebApi.Repositories
+{
+    public class JogoRepository
+    {
+        // possui acesso aos dados
+        private readonly LibraryContext _context;
+        // somente um data context na memória da aplicação na requisição, evitar o usar o new
+        // para o repositório existir, precisa do contexto, a aplicacao cria
+        // configurar no startup
+        public JogoRepository(LibraryContext context)
+        {
+            _context = context;
+        }
+        // retorna a lista de livros
+        public List<Jogo> Listar()
+        {
+            // SELECT Id, TituloDoJogo, LojaDisponivel , Disponivel FROM Livros;
+            return _context.Jogos.ToList();
+        }
+    }
+}
