3708c92bf4f08323ab8870938455dd1789096415
omartins-zs <gabrielmatheus209@gmail.com>
2022-02-05 18:28:19 -0300

    Criando Controllador Jogos

---
+++ SenaiLibrary.WebApi/Controllers/JogosController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SenaiLibrary.WebApi.Repositories;
+
+namespace SenaiLibrary.WebApi.Controllers
+{
+
+    /// summary
+    /// Controller responsável pelos endpoints URLs ) referentes aos livros
+    /// summary
+
+    // Define que o tipo de resposta da API será no formato JSON
+    [Produces("application/json")]
+
+    // Define que a rota de uma requisição será no formato dominio api nomeController
+    // ex : http://localhost:5000/api/livros
+    [Route("api/controller")]
+
+    // atributo para habilitar comportamentos especificos de API, como status, retorno
+    [ApiController]
+    // [ControllerBase]requisicoes HTTP
+
+    public class JogosController : ControllerBase
+    {
+        
+    }
+}
