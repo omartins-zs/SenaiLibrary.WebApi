0fb9e59d016a68a392eea66ef1d6227310c8ae79
omartins-zs <gabrielmatheus209@gmail.com>
2022-02-05 16:26:49 -0300

    Criando Model Jogo

--- SenaiLibrary.WebApi/Models/Jogo.cs
+++ SenaiLibrary.WebApi/Models/Jogo.cs
@@ -2,5 +2,11 @@
 {
     public class Jogo
     {
+        public int Id { get; set; }
+        public string TituloDoJogo { get; set; }
+        public string LojaDisponivel { get; set; }
+        public bool Disponivel { get; set; }
+
+
     }
 }
