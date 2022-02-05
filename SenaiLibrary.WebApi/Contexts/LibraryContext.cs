3261214ac22ddd814c8df9913099e6bbdf7f2b9e
omartins-zs <gabrielmatheus209@gmail.com>
2022-02-05 17:19:39 -0300

    Criando Pasta e Classe Context

---
+++ SenaiLibrary.WebApi/Contexts/LibraryContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SenaiLibrary.WebApi.Models;
+
+namespace SenaiLibrary.WebApi.Contexts
+{
+    // dbcontext é a ponte entre o modelo de classe e o banco de dados
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext()
+        { 
+
+        }
+        public LibraryContext(DbContextOptions<LibraryContext> options) : base(options)
+        {
+
+        }
+        // vamos utilizar esse método para configurar o banco de dados
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                // cada provedor tem sua sintaxe para especificação
+                optionsBuilder.UseSqlServer(("DataSource = DESKTOP-7JNQVD9t\\SQLEXPRESS; initial catalog = Chapter; Integrated Security = true"
+    }
+        }
+        //dbset representa as entidades que serão utilizadas nas operações de leitura, criação, atualização e deleção
+        public DbSet<Jogo> Jogos { get; set; }
+
+    }
+
+}
