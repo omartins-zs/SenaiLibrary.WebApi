5afccfd9fe0db9d2f2cd8fc6751a440a3c79c00c
omartins-zs <gabrielmatheus209@gmail.com>
2022-02-13 15:34:31 -0300

    Criando Model de Jogador

---
+++ SenaiLibrary.WebApi/Models/Jogador.cs
@@ -0,0 +1,9 @@
+﻿namespace SenaiLibrary.WebApi.Models
+{
+    public class Jogador
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+    }
+}
